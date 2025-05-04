89728a79d20f8c0f367987675ca750ee3f192b52
gupta-niharika <sanikchar@gmail.com>
2025-05-04 11:46:28 +0530

    v0.1.2 - railway procfile

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from router.search_router import router as search_router
 app = FastAPI(
     title="PDF Semantic Search API",
     description="Task for 8byte.ai",
-    version="0.1.1",
+    version="0.1.2",
 )
 
 app.include_router(search_router, prefix="/search")
